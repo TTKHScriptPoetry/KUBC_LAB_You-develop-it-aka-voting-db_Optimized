859e72a0b76c08ff4ad879485a4c87b39fa99cb7
=TTKHScriptPoetry <kimdotnetpossible@att.net>
2022-03-19 00:43:00 -0500

    Create API route for vote insertion and vote count per candidate

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-USE election;
+ Use election;
 -- DROP TABLE IF EXISTS parties;
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -6,21 +6,37 @@ CREATE TABLE parties (
   description TEXT
 );
 -- DROP TABLE IF EXISTS candidates;
+-- ON DELETE SET NULL = Tell SQL to set a candidate's party_id field of THIS candidate table to NULL 
+-- if the key from the reference table (id of parties) was deleted. 
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   party_id INTEGER,
   industry_connected BOOLEAN NOT NULL,
-  -- Tell SQL to set a candidate's party_id field of candidate table  to NULL
+ 
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL 
 );
 
+-- default specifies what the value should be if no value is provided.
 CREATE TABLE voters (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP -- default specifies what the value should be if no value is provided.
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP 
 );
+
+-- ON DELETE CASCADE = deleting the reference key voters-id will also delete the entire row from this table.
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+ 
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+)
+
 
